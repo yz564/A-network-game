1bad42c3d9b7beca3e232639d55f2b661e6a953c
yz566 <yz566@duke.edu>
2021-03-15 16:07:47 -0400

    documentation for maptextview

--- client/src/main/java/edu/duke/ece651/risk/client/MapTextView.java
+++ client/src/main/java/edu/duke/ece651/risk/client/MapTextView.java
@@ -7,12 +7,26 @@ import edu.duke.ece651.risk.shared.*;
 
 public class MapTextView {
 
+    /**
+     * The list of all players' names.
+     */
     private ArrayList<String> playerNames;
 
+    /**
+     * Constructs a MapTextView object with a given list of all players' name.
+     * 
+     * @param playerNames is the list of all players' names
+     */
     public MapTextView(ArrayList<String> playerNames) {
         this.playerNames = playerNames;
     }
 
+    /**
+     * Display the game's world map with text with a given WorldMap object.
+     * 
+     * @param toDisplay a WorldMap represents the game world map to display.
+     * @return a String that can be printed to display the world map.
+     */
     public String displayMap(WorldMap toDisplay) {
         String ans = "";
         for (String name : playerNames) {
@@ -21,6 +35,15 @@ public class MapTextView {
         return ans;
     }
 
+    /**
+     * Display the game's world map with text for one player's section with a given
+     * name and WorldMap object.
+     * 
+     * @param playerName Stirng the player's name. The player's section on the map
+     *                   will be displayed.
+     * @param toDisplay  a WorldMap represents the game world map to display.
+     * @return a String that can be printed to display the world map section.
+     */
     private String onePlayerSection(String playerName, WorldMap toDisplay) {
         String ans = playerName + ":\n-------------\n";
         HashSet<Territory> myTerritories = toDisplay.getPlayerTerritories(playerName);
@@ -30,6 +53,12 @@ public class MapTextView {
         return ans;
     }
 
+    /**
+     * Display one Territpry of the game's world map with text.
+     * 
+     * @param territory is a Territory object represents the territory to display.
+     * @return a String that can be printed to display the world map territory.
+     */
     private String oneTerritoryLine(Territory territory) {
         String ans = "";
         HashSet<Territory> neighbors = territory.getMyNeighbors();
