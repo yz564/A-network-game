7d68053b68257e7e244f367be6c163feb4ecc2c3
yz566 <yz566@duke.edu>
2021-03-16 17:08:14 -0400

    add executeMove method with 100% coverage

--- shared/src/test/java/edu/duke/ece651/risk/shared/ActionExecuterTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/ActionExecuterTest.java
@@ -21,4 +21,27 @@ public class ActionExecuterTest {
         assertEquals(281, map.getTerritory("Western Dothraki Sea").getNumUnits());
         assertEquals(100, map.getTerritory("Braavosian Coastlands").getNumUnits());
     }
+
+    @Test
+    public void test_execute_move() {
+        // create map
+        WorldMapFactory mf = new V1MapFactory();
+        WorldMap map = mf.makeWorldMap(3);
+        map.tryAssignInitOwner(1, "Green player");
+        map.tryAssignInitOwner(2, "Blue player");
+        map.tryAssignInitOwner(3, "Red player");
+        map.getTerritory("Western Dothraki Sea").trySetNumUnits(300);
+        map.getTerritory("Braavosian Coastlands").trySetNumUnits(100);
+
+        ActionExecuter executer = new ActionExecuter();
+        ActionInfo info1 = new ActionInfo("Western Dothraki Sea", "Braavosian Coastlands", 19);
+        executer.executeMove(map, info1);
+        assertEquals(281, map.getTerritory("Western Dothraki Sea").getNumUnits());
+        assertEquals(119, map.getTerritory("Braavosian Coastlands").getNumUnits());
+
+        ActionInfo info2 = new ActionInfo("Braavosian Coastlands", "Western Dothraki Sea", 19);
+        executer.executeMove(map, info2);
+        assertEquals(300, map.getTerritory("Western Dothraki Sea").getNumUnits());
+        assertEquals(100, map.getTerritory("Braavosian Coastlands").getNumUnits());
+    }
 }
