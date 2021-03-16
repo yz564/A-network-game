a7dddc0a93dd9e903141db0139dc6930f98b8f7d
yz566 <yz566@duke.edu>
2021-03-16 12:21:00 -0400

    add sendTroops method

--- shared/src/main/java/edu/duke/ece651/risk/shared/ActionExecuter.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/ActionExecuter.java
@@ -1,5 +1,31 @@
 package edu.duke.ece651.risk.shared;
 
 public class ActionExecuter {
+    /**
+     * Constructes a ActionExecuter helper class.
+     */
+    public ActionExecuter() {
+    }
+
+    /**
+     * Sends troops only from the src Territory, troops don't arrive dis Territory.
+     * 
+     * Info of src, dis, and troop to send is in ActionInfo info argument.
+     * 
+     * Validity of this action should be checked by rule checker, so this method
+     * doesn't throw error.
+     * 
+     * @param map  a WorldMap object where the action is implemented.
+     * @param info a ActionInfo object that contains the information of src, dis,
+     *             and troop to send.
+     */
+    public void sendTroops(WorldMap map, ActionInfo info) {
+        Territory src = map.getTerritory(info.getSrcName());
+        Territory dis = map.getTerritory(info.getDisName());
+        int sendNum = info.getUnitNum();
+
+        src.trySetNumUnits(src.getNumUnits() - sendNum);
+        dis.trySetNumUnits(dis.getNumUnits() + sendNum);
+    }
 
 }
