978f12e45da8756fe0a0301d0ec3347713c3e38a
Yang Zhong <yz564@duke.edu>
2021-03-18 14:54:15 -0400

    Comments added

--- server/src/main/java/edu/duke/ece651/risk/server/Player.java
+++ server/src/main/java/edu/duke/ece651/risk/server/Player.java
@@ -7,27 +7,35 @@ import java.net.Socket;
 
 import edu.duke.ece651.risk.shared.ObjectIO;
 
+/**
+ * client: the socket connected with the client
+ * name: the player's name
+ * isEnd to chech is the player lose 
+ * in and out are ObjectIOstream
+ * ready is important to control the player's thread, basic logic is writeObject once and then must readObject once...because of the blocking issue
+ * availableUnitNum: the total unit number a player have at the beginning of placement
+ * watch: a status indicates whether sending message to clients or not 
+ * tmp: used to store the most recently ObjectIO read in the player's thread.
+ */
 public class Player implements Runnable{
   private Socket client;
-  private int id;
   private String name;
   public volatile Boolean isEnd;
   public ObjectInputStream in;
   public ObjectOutputStream out;
   public volatile Boolean ready; //volatile: always updates this variable before being accessed by other threads
   public volatile int availableUnitNum;
-  // public volatile int unitNum;
   public Boolean watch;
   public ObjectIO tmp;
-
-  public Player(Socket client, int id,String name){
+  /**
+   * A simple constructor
+   */
+  public Player(Socket client,String name){
     this.client=client;
-    this.id=id;
     this.name = name;
     this.isEnd=false;
     this.ready = false;
     this.availableUnitNum=30;
-    //this.unitNum = 1;
   }
 
   public String getName() {
@@ -40,6 +48,9 @@ public class Player implements Runnable{
   public void setNotReady(){
       this.ready=false;
   }
+  /**
+   *must override run(), otherwise the thread will terminate soon.
+   */
   @Override
   public void run(){
     try{
@@ -51,9 +62,9 @@ public class Player implements Runnable{
     while(!isEnd){
       try{
         System.out.println("-----waitClientInput------");
-        if ((tmp=(ObjectIO)in.readObject())!=null){
+        if ((tmp=(ObjectIO)in.readObject())!=null){ //blocked when waiting for a Object sent from the client
           System.out.println("-----ReadClientInput------");
-          ready=true;
+          ready=true; //let the main thread know this player's thread is ready for sending next object.
         }
       }
       catch(Exception e){}
