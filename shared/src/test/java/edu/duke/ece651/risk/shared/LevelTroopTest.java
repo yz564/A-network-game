c7276b9c4c4e67cbaa559996131cb05f026bb279
yz566 <yz566@duke.edu>
2021-03-24 23:46:03 -0400

    fix test cases

--- shared/src/test/java/edu/duke/ece651/risk/shared/LevelTroopTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/LevelTroopTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LevelTroopTest {
     @Test
     public void test_constructors(){
-        Troop at1 = new LevelTroop("level1", 0, 99999, 0, 0);
+        Troop at1 = new LevelTroop("level1", 0, 99999, 0, 0, 0);
         Troop at2 = new LevelTroop("level2", 0, 3, 1, 5);
         assertEquals(0, at1.getBonus());
         assertEquals(3, at2.getBonus());
