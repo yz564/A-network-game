7910956b827af8c2a4a30ee228625d141b1ec982
jx97 <jx97@duke.edu>
2021-03-27 13:55:12 -0400

    edited Troop, AbstractTroop, BasicTroop, LevelTroop formatting, added isValidUnits() for checking if units is valid (>=0 and <=UnitLimit)

--- shared/src/main/java/edu/duke/ece651/risk/shared/BasicTroop.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/BasicTroop.java
@@ -1,63 +1,63 @@
 package edu.duke.ece651.risk.shared;
 
-/*
+/**
  * A troop contains an arbitrary number of a particular kind of units. E.g. a
  * troop could contain 10 soldiers or 5 knights but not both.
  */
 public class BasicTroop extends AbstractTroop {
+  private static final long serialVersionUID = -1788807224514071854L;
 
-    /*
-     * Constructs a BasicTroop object, with name filed assigned "Basic".
-     *
-     * @param numUnits is the number of units put in the troop.
-     *
-     * @param unitLimit is the maximum amount of units that a troop can contain.
-     */
-    public BasicTroop(int numUnits, int unitLimit) {
-        super("Basic", numUnits, unitLimit);
-    }
+  /**
+   * Constructs a BasicTroop object, with name filed assigned "Basic".
+   *
+   * @param numUnits  is the number of units put in the troop.
+   * @param unitLimit is the maximum amount of units that a troop can contain.
+   */
+  public BasicTroop(int numUnits, int unitLimit) {
+    super("Basic", numUnits, unitLimit);
+  }
 
-    /*
-     * Construct a BasicTroop object. The default number of units is 999 if numLimit
-     * is not explicitly set.
-     *
-     * @param numUnits is the number of units put in the troop.
-     */
-    public BasicTroop(int numUnits) {
-        this(numUnits, 99999);
-    }
+  /**
+   * Construct a BasicTroop object. The default number of units is 999 if numLimit
+   * is not explicitly set.
+   *
+   * @param numUnits is the number of units put in the troop.
+   */
+  public BasicTroop(int numUnits) {
+    this(numUnits, 99999);
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o != null && o.getClass().equals(getClass())) {
-            BasicTroop other = (BasicTroop) o;
-            return this.troopName == other.getName() && this.numUnits == other.getNumUnits();
-        }
-        return false;
+  @Override
+  public boolean equals(Object o) {
+    if (o != null && o.getClass().equals(getClass())) {
+      BasicTroop other = (BasicTroop) o;
+      return this.troopName == other.getName() && this.numUnits == other.getNumUnits();
     }
+    return false;
+  }
 
-    @Override
-    public String toString() {
-        return this.troopName + " with " + this.numUnits + " units.\n";
-    }
+  @Override
+  public String toString() {
+    return this.troopName + " with " + this.numUnits + " units.\n";
+  }
 
-    @Override
-    public int hashCode() {
-        return toString().hashCode();
-    }
+  @Override
+  public int hashCode() {
+    return toString().hashCode();
+  }
 
-    @Override
-    public int getBonus() {
-        return 0;
-    }
+  @Override
+  public int getBonus() {
+    return 0;
+  }
 
-    @Override
-    public int getTechLevelReq() {
-        return 0;
-    }
+  @Override
+  public int getTechLevelReq() {
+    return 0;
+  }
 
-    @Override
-    public int getTechCost() {
-        return 0;
-    }
+  @Override
+  public int getTechCost() {
+    return 0;
+  }
 }
--- shared/src/main/java/edu/duke/ece651/risk/shared/Troop.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/Troop.java
@@ -1,61 +1,77 @@
 package edu.duke.ece651.risk.shared;
 
-/*
+/**
  * A troop contains an arbitrary number of a particular kind of units. E.g. a
  * troop could contain 10 soldiers or 5 knights but not both.
  */
 public interface Troop extends java.io.Serializable {
-    /*
-     * Add units to the troop.
-     *
-     * @param toAdd is the number of units to add.
-     *
-     * @return true is adding is successful, else returns false.
-     */
-    public boolean tryAddUnits(int toAdd);
 
-    /*
-     * Remove units from the troop.
-     *
-     * @param toRemove is the number of units to remove.
-     *
-     * @return true is removal is successful, else returns false.
-     */
-    public boolean tryRemoveUnits(int toRemove);
+  /**
+   * Return troop name.
+   */
+  public String getName();
 
-    /*
-     * Return number of units in the troop.
-     */
-    public int getNumUnits();
+  /**
+   * Return number of units in the troop.
+   */
+  public int getNumUnits();
 
-    /* Return troop name.
-     */
-    public String getName();
+  /**
+   * Check if the number of units is valid for a troop.
+   * 
+   * @param toCheck is the number of units to check if is valid for this troop.
+   *
+   * @return true if the number of units is valid, else returns false.
+   */
+  public boolean isValidUnits(int toCheck);
 
-    /* Set the number of units in a troop.
-     * @param numUnits is the number of units that this troop will be assigned.
-     */
-    public boolean trySetNumUnits(int numUnits);
+  /**
+   * Set the number of units in a troop. Checks if the number of units is valid
+   * before assignment.
+   * 
+   * @param numUnits is the number of units that this troop will be assigned.
+   *
+   * @return true if the setting is successful, else returns false.
+   */
+  public boolean trySetNumUnits(int numUnits);
 
-    /**
-     * Get the bonus amount of the units in the Troop.
-     *
-     * @return an int that represents the bonus amount for attacking.
-     */
-    public int getBonus();
+  /**
+   * Add units to the troop. Checks if the resulting unit is valid before adding.
+   *
+   * @param toAdd is the number of units to add.
+   *
+   * @return true if adding is successful, else returns false.
+   */
+  public boolean tryAddUnits(int toAdd);
 
-    /**
-     * Get the tech level required for using this Troop.
-     *
-     * @return an int that represents tech level required.
-     */
-    public int getTechLevelReq();
+  /**
+   * Remove units from the troop. Checks if the resulting unit is valid before removing.
+   *
+   * @param toRemove is the number of units to remove.
+   *
+   * @return true if removal is successful, else returns false.
+   */
+  public boolean tryRemoveUnits(int toRemove);
 
-    /**
-     * Get the tech cost needed to upgrade a unit from troop that is 1 level lower to a unit in this
-     * troop.
-     *
-     * @return int that represents the total cost.
-     */
-    public int getTechCost();
+  /**
+   * Get the bonus amount of the units in the Troop.
+   *
+   * @return an int that represents the bonus amount for attacking.
+   */
+  public int getBonus();
+
+  /**
+   * Get the tech level required for using this Troop.
+   *
+   * @return an int that represents tech level required.
+   */
+  public int getTechLevelReq();
+
+  /**
+   * Get the tech cost needed to upgrade a unit from troop that is 1 level lower
+   * to a unit in this troop.
+   *
+   * @return int that represents the total cost.
+   */
+  public int getTechCost();
 }
