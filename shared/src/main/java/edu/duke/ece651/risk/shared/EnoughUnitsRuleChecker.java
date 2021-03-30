a937108e6b7c2130252e8c0cbc10071074648eac
jx97 <jx97@duke.edu>
2021-03-30 10:50:26 -0400

    changed SrcValidityRuleChecker to EnoughUnitsRuleChecker: checks for enough units for all levels of troops

---
+++ shared/src/main/java/edu/duke/ece651/risk/shared/EnoughUnitsRuleChecker.java
@@ -0,0 +1,41 @@
+package edu.duke.ece651.risk.shared;
+
+import java.util.HashMap;
+
+/**
+ * Subclass to check if source territory contains enough units of specified
+ * troop name to perform the action.
+ */
+public class EnoughUnitsRuleChecker extends ActionRuleChecker {
+
+  /**
+   * Constructs a EnoughUnitsRuleChecker
+   *
+   * @param next is the ActionRuleChecker to be passed to the super class's
+   *             constructor
+   */
+  public EnoughUnitsRuleChecker(ActionRuleChecker next) {
+    super(next);
+  }
+
+  /**
+   * Checks if the source territory contains enough units of specified troop name
+   * in the source territory to perform the action. Violates rule if any of the
+   * specified troop names does not contain enough units.
+   *
+   * @param action   is the ActionInfo for an action
+   * @param worldmap is the worldmap to perform the action on
+   * @return error message if violates action rules, else null
+   */
+  @Override
+  protected String checkMyRule(ActionInfo action, WorldMap worldmap) {
+    Territory src = worldmap.getTerritory(action.getSrcName());
+    HashMap<String, Integer> numUnits = action.getNumUnits();
+    for (String troopName : numUnits.keySet()) {
+      if (src.getTroopNumUnits(troopName) < numUnits.get(troopName)) {
+        return "That action is invalid: source Territory does not contain enough " + troopName + " units";
+      }
+    }
+    return null;
+  }
+}
--- shared/src/test/java/edu/duke/ece651/risk/shared/EnoughUnitsRuleCheckerTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/EnoughUnitsRuleCheckerTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 
-public class SrcValidityRuleCheckerTest {
+public class EnoughUnitsRuleCheckerTest {
     @Test
     public void test_srcvalidity() {
-        SrcValidityRuleChecker rc = new SrcValidityRuleChecker(null);
+        EnoughUnitsRuleChecker rc = new EnoughUnitsRuleChecker(null);
         WorldMapFactory factory = new V1MapFactory();
         WorldMap worldmap = factory.makeTestWorldMap();
         worldmap.tryAssignInitOwner(1, "Player 1");
@@ -22,17 +22,13 @@ public class SrcValidityRuleCheckerTest {
         HashMap<String, Integer> unitNum1 = new HashMap<>();
         unitNum1.put("Basic", 3);
         ActionInfoFactory af = new ActionInfoFactory();
-        ActionInfo a1 = af.createMoveActionInfo("Player 1", "Narnia", "Midkemia", unitNum1);
-        ActionInfo a2 = af.createMoveActionInfo("Player 1", "Elantris", "Midkemia", unitNum1);
+        //ActionInfo a1 = af.createMoveActionInfo("Player 1", "Narnia", "Midkemia", unitNum1);
+        //ActionInfo a2 = af.createMoveActionInfo("Player 1", "Elantris", "Midkemia", unitNum1);
         HashMap<String, Integer> unitNum2 = new HashMap<>();
         unitNum2.put("Basic", 11);
         ActionInfo a3 = af.createMoveActionInfo("Player 1", "Narnia", "Midkemia", unitNum2);
-        assertNull(rc.checkMyRule(a1, worldmap));
         assertEquals(
-                "That action is invalid: source Territory belong to a different player",
-                rc.checkMyRule(a2, worldmap));
-        assertEquals(
-                "That action is invalid: source Territory does not contain enough units",
+                "That action is invalid: source Territory does not contain enough Basic units",
                 rc.checkMyRule(a3, worldmap));
     }
 }
