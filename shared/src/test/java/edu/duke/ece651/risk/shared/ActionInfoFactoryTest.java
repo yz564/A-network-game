3a778967d990d4bb789370908dd8ddabb7bf2d27
yz566 <yz566@duke.edu>
2021-03-29 20:38:25 -0400

    test territory_action_info

---
+++ shared/src/test/java/edu/duke/ece651/risk/shared/ActionInfoFactoryTest.java
@@ -0,0 +1,13 @@
+package edu.duke.ece651.risk.shared;
+
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActionInfoFactoryTest {
+    @Test
+    public void test_methods() {
+        ActionInfoFactory af = new ActionInfoFactory();
+    }
+}
--- shared/src/test/java/edu/duke/ece651/risk/shared/ActionInfoTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/ActionInfoTest.java
@@ -1,5 +1,6 @@
 package edu.duke.ece651.risk.shared;
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -8,14 +9,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ActionInfoTest {
     @Test
-    public void test_getters() {
+    public void test_default_constructor() {
         ActionInfo info = new ActionInfo("AAA", "move");
         assertEquals("AAA", info.getSrcOwnerName());
         assertEquals("move", info.getActionType());
     }
 
     @Test
-    public void test_setters() {
-        ActionInfo info = new ActionInfo("AAA", "move");
+    public void test_territory_action_constructor() {
+        HashMap<String, Integer> unitNum = new HashMap<String, Integer>();
+        unitNum.put("level0", 3);
+        TerritoryActionInfo infoDetail = new TerritoryActionInfo("a", "b", unitNum);
+        ActionInfo info = new ActionInfo("Player 1", "move", infoDetail);
+        assertEquals("a", info.getTerritoryActionInfo().getSrcName());
+
+        assertNull(info.getUpgradeTechActionInfo());
+        assertNull(info.getUpgradeUnitActionInfo());
+    }
+
+    @Test
+    public void test_upgrade_unit_action_constructor() {
+        UpgradeUnitActionInfo infoDetail = new UpgradeUnitActionInfo("1", 3, 5, 10);
+        ActionInfo info = new ActionInfo("Player 1", "upgrade unit", infoDetail);
+        assertEquals(10, info.getUpgradeUnitActionInfo().getNumToUpgrade());
+    }
+
+    @Test
+    public void test_upgrade_tech_action_constructor() {
+        UpgradeTechActionInfo infoDetail = new UpgradeTechActionInfo(1, 3);
+        ActionInfo info = new ActionInfo("Player 1", "upgrade tech", infoDetail);
+        assertEquals(3, info.getUpgradeTechActionInfo().getNewTechLevel());
     }
 }
