06899478559ed7aa6a3e72af8103a04340a22faf
jx97 <jx97@duke.edu>
2021-03-30 11:04:53 -0400

    updated rule checker tests for changes in action info, and changed to V2MapFactory

--- shared/src/test/java/edu/duke/ece651/risk/shared/DesReachableRuleCheckerTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/DesReachableRuleCheckerTest.java
@@ -7,24 +7,23 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 
 public class DesReachableRuleCheckerTest {
-    @Test
-    public void test_desreachable() {
-        DesReachableRuleChecker rc = new DesReachableRuleChecker(null);
-        WorldMapFactory factory = new V1MapFactory();
-        WorldMap worldmap = factory.makeTestWorldMap();
-        worldmap.tryAssignInitOwner(1, "Player 1");
-        worldmap.tryAssignInitOwner(2, "Player 2");
-        worldmap.tryAssignInitOwner(3, "Player 3");
-        HashMap<String, Integer> unitNum1 = new HashMap<>();
-        unitNum1.put("level0", 3);
-        ActionInfoFactory af = new ActionInfoFactory();
-        ActionInfo a1 = af.createMoveActionInfo("Player 1", "Narnia", "Oz", unitNum1);
-        HashMap<String, Integer> unitNum2 = new HashMap<>();
-        unitNum2.put("level0", 3);
-        ActionInfo a2 = af.createMoveActionInfo("Player 1", "Narnia", "Hogwarts", unitNum2);
-        assertNull(rc.checkMyRule(a1, worldmap));
-        assertEquals(
-                "That action is invalid: destination Territory is not reachable from source Territory",
-                rc.checkMyRule(a2, worldmap));
-    }
+  @Test
+  public void test_desreachable() {
+    DesReachableRuleChecker rc = new DesReachableRuleChecker(null);
+    WorldMapFactory factory = new V2MapFactory();
+    WorldMap worldmap = factory.makeTestWorldMap();
+    worldmap.tryAssignInitOwner(1, "Player 1");
+    worldmap.tryAssignInitOwner(2, "Player 2");
+    worldmap.tryAssignInitOwner(3, "Player 3");
+    HashMap<String, Integer> unitNum1 = new HashMap<>();
+    unitNum1.put("level0", 3);
+    ActionInfoFactory af = new ActionInfoFactory();
+    ActionInfo a1 = af.createMoveActionInfo("Player 1", "Narnia", "Oz", unitNum1);
+    HashMap<String, Integer> unitNum2 = new HashMap<>();
+    unitNum2.put("level0", 3);
+    ActionInfo a2 = af.createMoveActionInfo("Player 1", "Narnia", "Hogwarts", unitNum2);
+    assertNull(rc.checkMyRule(a1, worldmap));
+    assertEquals("That action is invalid: destination Territory is not reachable from source Territory",
+        rc.checkMyRule(a2, worldmap));
+  }
 }
--- shared/src/test/java/edu/duke/ece651/risk/shared/EnoughUnitsRuleCheckerTest.java
+++ shared/src/test/java/edu/duke/ece651/risk/shared/EnoughUnitsRuleCheckerTest.java
@@ -7,28 +7,25 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 
 public class EnoughUnitsRuleCheckerTest {
-    @Test
-    public void test_srcvalidity() {
-        EnoughUnitsRuleChecker rc = new EnoughUnitsRuleChecker(null);
-        WorldMapFactory factory = new V1MapFactory();
-        WorldMap worldmap = factory.makeTestWorldMap();
-        worldmap.tryAssignInitOwner(1, "Player 1");
-        worldmap.tryAssignInitOwner(2, "Player 2");
-        worldmap.tryAssignInitOwner(3, "Player 3");
-        Territory t1 = worldmap.getTerritory("Narnia");
-        Territory t2 = worldmap.getTerritory("Midkemia");
-        t1.tryAddTroopUnits("Basic", 10);
-        t2.tryAddTroopUnits("Basic", 12);
-        HashMap<String, Integer> unitNum1 = new HashMap<>();
-        unitNum1.put("Basic", 3);
-        ActionInfoFactory af = new ActionInfoFactory();
-        //ActionInfo a1 = af.createMoveActionInfo("Player 1", "Narnia", "Midkemia", unitNum1);
-        //ActionInfo a2 = af.createMoveActionInfo("Player 1", "Elantris", "Midkemia", unitNum1);
-        HashMap<String, Integer> unitNum2 = new HashMap<>();
-        unitNum2.put("Basic", 11);
-        ActionInfo a3 = af.createMoveActionInfo("Player 1", "Narnia", "Midkemia", unitNum2);
-        assertEquals(
-                "That action is invalid: source Territory does not contain enough Basic units",
-                rc.checkMyRule(a3, worldmap));
-    }
+  @Test
+  public void test_srcvalidity() {
+    EnoughUnitsRuleChecker rc = new EnoughUnitsRuleChecker(null);
+    WorldMapFactory factory = new V2MapFactory();
+    WorldMap worldmap = factory.makeTestWorldMap();
+    worldmap.tryAssignInitOwner(1, "Player 1");
+    worldmap.tryAssignInitOwner(2, "Player 2");
+    worldmap.tryAssignInitOwner(3, "Player 3");
+    Territory t1 = worldmap.getTerritory("Narnia");
+    Territory t2 = worldmap.getTerritory("Midkemia");
+    t1.tryAddTroopUnits("level0", 10);
+    t2.tryAddTroopUnits("level0", 12);
+    HashMap<String, Integer> unitNum1 = new HashMap<>();
+    unitNum1.put("level0", 3);
+    ActionInfoFactory af = new ActionInfoFactory();
+    HashMap<String, Integer> unitNum2 = new HashMap<>();
+    unitNum2.put("level0", 11);
+    ActionInfo a3 = af.createMoveActionInfo("Player 1", "Narnia", "Midkemia", unitNum2);
+    assertEquals("That action is invalid: source Territory does not contain enough level0 units",
+        rc.checkMyRule(a3, worldmap));
+  }
 }
