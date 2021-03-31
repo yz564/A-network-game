6dc4178cbd9ce3465810ddd29725881fe0c6361e
jx97 <jx97@duke.edu>
2021-03-31 00:27:40 -0400

    created ActionRuleCheckerHelper class

---
+++ shared/src/main/java/edu/duke/ece651/risk/shared/ActionRuleCheckerHelper.java
@@ -0,0 +1,70 @@
+package edu.duke.ece651.risk.shared;
+
+public class ActionRuleCheckerHelper {
+  private final ActionRuleChecker moveChecker;
+  private final ActionRuleChecker attackChecker;
+  private final ActionRuleChecker upgradeUnitChecker;
+  private final ActionRuleChecker upgradeTechChecker;
+
+  /**
+   * Constructs an ActionRuleCheckerHelper, by initializing a ActionRuleChecker
+   * for each type of actions via respective rule checker chains.
+   */
+  public ActionRuleCheckerHelper() {
+    this.moveChecker = new TerritoryExistenceRuleChecker(new SrcOwnershipRuleChecker(
+        new TroopExistenceRuleChecker(new EnoughUnitsRuleChecker(new DesReachableRuleChecker(null)))));
+    this.attackChecker = new TerritoryExistenceRuleChecker(new SrcOwnershipRuleChecker(new TroopExistenceRuleChecker(
+        new EnoughUnitsRuleChecker(new DesOwnershipRuleChecker(new DesAdjacencyRuleChecker(null))))));
+    this.upgradeUnitChecker = new TerritoryExistenceRuleChecker(new SrcOwnershipRuleChecker(
+        new TroopExistenceRuleChecker(new EnoughUnitsRuleChecker(new UpgradeUnitRuleChecker(null)))));
+    this.upgradeTechChecker = new UpgradeTechRuleChecker(null);
+  }
+
+  /**
+   * Checks if all the action rules are met to perform a move action on a given
+   * worldmap. Calls the checkMyRule() of moveChecker.
+   * 
+   * @param action   is the ActionInfo for an action
+   * @param worldmap is the worldmap to perform the action on
+   * @return error message if violates action rules, else null
+   */
+  public String checkRuleForMove(ActionInfo action, WorldMap worldmap) {
+    return this.moveChecker.checkMyRule(action, worldmap);
+  }
+
+  /**
+   * Checks if all the action rules are met to perform an attack action on a given
+   * worldmap. Calls the checkMyRule() of attackChecker.
+   * 
+   * @param action   is the ActionInfo for an action
+   * @param worldmap is the worldmap to perform the action on
+   * @return error message if violates action rules, else null
+   */
+  public String checkRuleForAttack(ActionInfo action, WorldMap worldmap) {
+    return this.attackChecker.checkMyRule(action, worldmap);
+  }
+
+  /**
+   * Checks if all the action rules are met to perform an upgrade unit action on a
+   * given worldmap. Calls the checkMyRule() of upgradeUnitChecker.
+   * 
+   * @param action   is the ActionInfo for an action
+   * @param worldmap is the worldmap to perform the action on
+   * @return error message if violates action rules, else null
+   */
+  public String checkRuleForUpgradeUnit(ActionInfo action, WorldMap worldmap) {
+    return this.upgradeUnitChecker.checkMyRule(action, worldmap);
+  }
+
+  /**
+   * Checks if all the action rules are met to perform an upgrade tech action on a
+   * given worldmap. Calls the checkMyRule() of upgradeTechChecker.
+   * 
+   * @param action   is the ActionInfo for an action
+   * @param worldmap is the worldmap to perform the action on
+   * @return error message if violates action rules, else null
+   */
+  public String checkRuleForUpgradeTech(ActionInfo action, WorldMap worldmap) {
+    return this.upgradeTechChecker.checkMyRule(action, worldmap);
+  }
+}
