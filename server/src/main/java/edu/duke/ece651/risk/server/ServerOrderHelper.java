21d022ea8dee55a2d9cf79e97c0d64395a4171c8
yz566 <yz566@duke.edu>
2021-04-02 19:17:15 -0400

    rehearse group 1 orders

--- server/src/main/java/edu/duke/ece651/risk/server/ServerOrderHelper.java
+++ server/src/main/java/edu/duke/ece651/risk/server/ServerOrderHelper.java
@@ -6,161 +6,175 @@ import org.apache.commons.lang3.SerializationUtils;
 import edu.duke.ece651.risk.shared.*;
 
 public class ServerOrderHelper {
-  /**
-   * The rule checker for move action.
-   */
-  private ActionRuleChecker moveChecker;
+    /** The rule checker helper for actions. */
+    private final ActionRuleCheckerHelper ruleChecker = new ActionRuleCheckerHelper();
 
-  /**
-   * The rule checker for attack action.
-   */
-  private ActionRuleChecker attackChecker;
+    /**
+     * An ArrayList of ActionInfo that represents all information needed to execute the attack
+     * action.
+     */
+    public ArrayList<ActionInfo> attackOrders;
 
-  /**
-   * An ArrayList of ActionInfo that represents all information needed to execute
-   * the attack action.
-   */
-  public ArrayList<ActionInfo> attackOrders;
+    /**
+     * An ArrayList of ActionInfo that represents all information needed to execute the move action,
+     * upgrade tech action, or upgrade unit action.
+     */
+    public ArrayList<ActionInfo> group1Orders;
 
-  /**
-   * An ArrayList of ActionInfo that represents all information needed to execute
-   * the move action.
-   */
-  public ArrayList<ActionInfo> moveOrders;
+    /** The executer for execute attack and move actions. */
+    private final ActionExecuter executer;
 
-  /**
-   * The executer for execute attack and move actions.
-   */
-  private ActionExecuter executer;
-
-  /**
-   * Getter of attackOrders
-   * 
-   * @return an ArrayList of ActionInfo that represents all information needed to
-   *         execute the attack action.
-   */
-  public ArrayList<ActionInfo> getAttackOrders() {
-    return this.attackOrders;
-  }
-
-  /**
-   * Getter of moveOrders.
-   * 
-   * @return an ArrayList of ActionInfo that represents all information needed to
-   *         execute the move action.
-   */
-  public ArrayList<ActionInfo> getMoveOrders() {
-    return this.moveOrders;
-  }
-
-  /**
-   * Default constructor of the ServerOrderHelper.
-   */
-  public ServerOrderHelper() {
-    this.moveChecker = new TerritoryExistenceRuleChecker(new SrcOwnershipRuleChecker(
-        new TroopExistenceRuleChecker(new EnoughUnitsRuleChecker(new DesReachableRuleChecker(null)))));
-    this.attackChecker = new TerritoryExistenceRuleChecker(new SrcOwnershipRuleChecker(new TroopExistenceRuleChecker(
-        new EnoughUnitsRuleChecker(new DesOwnershipRuleChecker(new DesAdjacencyRuleChecker(null))))));
-    this.attackOrders = new ArrayList<ActionInfo>();
-    this.moveOrders = new ArrayList<ActionInfo>();
-    this.executer = new ActionExecuter(); // default seed
-  }
-
-  /**
-   * Contructs a ServerOrderHelper with a given random seed for executing attack
-   * action.
-   * 
-   * @param seed the random seed for executing attack action.
-   */
-  public ServerOrderHelper(long seed) {
-    this.moveChecker = new TerritoryExistenceRuleChecker(new SrcOwnershipRuleChecker(
-        new TroopExistenceRuleChecker(new EnoughUnitsRuleChecker(new DesReachableRuleChecker(null)))));
-    this.attackChecker = new TerritoryExistenceRuleChecker(new SrcOwnershipRuleChecker(new TroopExistenceRuleChecker(
-        new EnoughUnitsRuleChecker(new DesOwnershipRuleChecker(new DesAdjacencyRuleChecker(null))))));
-    this.attackOrders = new ArrayList<ActionInfo>();
-    this.moveOrders = new ArrayList<ActionInfo>();
-    this.executer = new ActionExecuter(seed);
-  }
-
-  /*
-   * Clear all saved orders in the helper. Should be Calles after executes all
-   * orders if this helper needs to be used for another round of orders.
-   */
-  public void clearAllOrders() {
-    attackOrders = new ArrayList<ActionInfo>();
-    moveOrders = new ArrayList<ActionInfo>();
-  }
-
-  /**
-   * Collects orders got from all player clients.
-   * 
-   * @param orders the ObjectIO object that contains all orders.
-   */
-  public void collectOrders(ObjectIO orders) {
-    for (ActionInfo info : orders.moveOrders) {
-      moveOrders.add(info);
+    /**
+     * Getter of attackOrders
+     *
+     * @return an ArrayList of ActionInfo that represents all information needed to execute the
+     *     attack action.
+     */
+    public ArrayList<ActionInfo> getAttackOrders() {
+        return this.attackOrders;
     }
-    for (ActionInfo info : orders.attackOrders) {
-      attackOrders.add(info);
-    }
-  }
 
-  /**
-   * Try to resolve all move orders in this helper. Orders will all be resolved if
-   * there is no problem in the orders. If there is problem in at least one of the
-   * orders, no order will be resolved.
-   * 
-   * @param map the WorldMap on which server executes the actions.
-   * @return a String describing the problem in the orders. If there is no
-   *         problem, returns null.
-   */
-  public String tryResolveMoveOrders(WorldMap map) {
-    WorldMap temp = (WorldMap) SerializationUtils.clone(map); // temp map for checking move action
-    String problem = null;
-    for (ActionInfo order : moveOrders) {
-      problem = moveChecker.checkAction(order, temp); // Do a check
-      if (problem != null) {
+    /**
+     * Getter of group1Orders.
+     *
+     * <p>An ArrayList of ActionInfo that represents all information needed to execute the move
+     * action, upgrade tech action, or upgrade unit action.
+     */
+    public ArrayList<ActionInfo> group1Orders() {
+        return this.group1Orders;
+    }
+
+    /** Default constructor of the ServerOrderHelper. */
+    public ServerOrderHelper() {
+        this.attackOrders = new ArrayList<ActionInfo>();
+        this.group1Orders = new ArrayList<ActionInfo>();
+        this.executer = new ActionExecuter(); // default seed
+    }
+
+    /**
+     * Constructs a ServerOrderHelper with a given random seed for executing attack action.
+     *
+     * @param seed the random seed for executing attack action.
+     */
+    public ServerOrderHelper(long seed) {
+        this.attackOrders = new ArrayList<ActionInfo>();
+        this.group1Orders = new ArrayList<ActionInfo>();
+        this.executer = new ActionExecuter(seed);
+    }
+
+    /*
+     * Clear all saved orders in the helper. Should be Called after executes all
+     * orders if this helper needs to be used for another round of orders.
+     */
+    public void clearAllOrders() {
+        attackOrders = new ArrayList<ActionInfo>();
+        group1Orders = new ArrayList<ActionInfo>();
+    }
+
+    /**
+     * Collects orders got from all player clients.
+     *
+     * @param orders the ObjectIO object that contains all orders.
+     */
+    public void collectOrders(ObjectIO orders) {
+        group1Orders.addAll(orders.moveOrders);
+        attackOrders.addAll(orders.attackOrders);
+    }
+
+    private String rehearseGroup1Orders(WorldMap tempMap) {
+        for (ActionInfo order : group1Orders) {
+            if (order.getActionType().equals("move")) {
+                String problem = ruleChecker.checkRuleForMove(order, tempMap); // Do a check
+                if (problem != null) {
+                    return problem;
+                } else {
+                    executer.executeMove(tempMap, order); // execute a move on temp map
+                }
+            } else if (order.getActionType().equals("upgrade tech")) {
+                String problem = ruleChecker.checkRuleForUpgradeTech(order, tempMap);
+                if (problem != null) {
+                    return problem;
+                } else {
+                    executer.executeUpgradeTech(tempMap, order);
+                }
+            } else {
+                String problem = ruleChecker.checkRuleForUpgradeUnit(order, tempMap);
+                if (problem != null) {
+                    return problem;
+                } else {
+                    executer.executeUpgradeUnit(tempMap, order);
+                }
+            }
+        }
+        return null;
+    }
+
+    private void resolveGroup1Orders(WorldMap map) {
+        for (ActionInfo order : group1Orders) {
+            if (order.getActionType().equals("move")) {
+                executer.executeMove(map, order); // execute a move on temp map
+            } else if (order.getActionType().equals("upgrade tech")) {
+                executer.executeUpgradeTech(map, order);
+            } else {
+                executer.executeUpgradeUnit(map, order);
+            }
+        }
+    }
+
+    /**
+     * Try to resolve all group1 orders in this helper. Orders will all be resolved if there is no
+     * problem in the orders. If there is problem in at least one of the orders, no order will be
+     * resolved.
+     *
+     * @param map the WorldMap on which server executes the actions.
+     * @return a String describing the problem in the orders. If there is no problem, returns null.
+     */
+    public String tryResolveMoveOrders(WorldMap map) {
+        WorldMap temp =
+                (WorldMap) SerializationUtils.clone(map); // temp map for checking move action
+        String problem = null;
+        for (ActionInfo order : group1Orders) {
+            problem = ruleChecker.checkRuleForMove(order, temp); // Do a check
+            if (problem != null) {
+                return problem;
+            } else {
+                executer.executeMove(temp, order); // execute a move on temp map
+            }
+        }
+        // real executions
+        for (ActionInfo order : group1Orders) {
+            executer.executeMove(map, order);
+        }
         return problem;
-      } else {
-        executer.executeMove(temp, order); // execute a move on temp map
-      }
     }
-    // real executions
-    for (ActionInfo order : moveOrders) {
-      executer.executeMove(map, order);
-    }
-    return problem;
-  }
 
-  /**
-   * Try to resolve all attack orders in this helper. Orders will all be resolved
-   * if there is no problem in the orders. If there is problem in at least one of
-   * the orders, no order will be resolved.
-   * 
-   * @param map the WorldMap on which server executes the actions.
-   * @return a String describing the problem in the orders. If there is no
-   *         problem, returns null.
-   */
-  public String tryResolveAttackOrders(WorldMap map) {
-    WorldMap temp = (WorldMap) SerializationUtils.clone(map);
-    String problem = null;
-    for (ActionInfo order : attackOrders) {
-      problem = attackChecker.checkAction(order, temp);
-      if (problem != null) {
+    /**
+     * Try to resolve all attack orders in this helper method. Orders will all be resolved if there
+     * is no problem in the orders. If there is problem in at least one of the orders, no order will
+     * be resolved.
+     *
+     * @param map the WorldMap on which server executes the actions.
+     * @return a String describing the problem in the orders. If there is no problem, returns null.
+     */
+    public String tryResolveAttackOrders(WorldMap map) {
+        WorldMap temp = (WorldMap) SerializationUtils.clone(map);
+        String problem = null;
+        for (ActionInfo order : attackOrders) {
+            problem = ruleChecker.checkRuleForAttack(order, temp);
+            if (problem != null) {
+                return problem;
+            } else {
+                executer.sendTroops(temp, order);
+                executer.executeAttack(temp, order);
+            }
+        }
+        // real executions
+        for (ActionInfo order : attackOrders) {
+            executer.sendTroops(map, order);
+        }
+        for (ActionInfo order : attackOrders) {
+            executer.executeAttack(map, order);
+        }
         return problem;
-      } else {
-        executer.sendTroops(temp, order);
-        executer.executeAttack(temp, order);
-      }
     }
-    // real executions
-    for (ActionInfo order : attackOrders) {
-      executer.sendTroops(map, order);
-    }
-    for (ActionInfo order : attackOrders) {
-      executer.executeAttack(map, order);
-    }
-    return problem;
-  }
-
 }
