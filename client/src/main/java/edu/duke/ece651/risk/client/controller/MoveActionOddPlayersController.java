6df2036253aa7ad00b7dcddeb4cb0c83a72b42c3
jx97 <jy.xu@duke.edu>
2021-04-06 13:36:00 -0400

    added playerinfo to all action sub views

--- client/src/main/java/edu/duke/ece651/risk/client/controller/MoveActionOddPlayersController.java
+++ client/src/main/java/edu/duke/ece651/risk/client/controller/MoveActionOddPlayersController.java
@@ -25,169 +25,168 @@ import java.util.HashMap;
 import java.util.ResourceBundle;
 
 public class MoveActionOddPlayersController implements Initializable {
-    App model;
-    String next;
-    ObservableList territoryNames = FXCollections.observableArrayList();
+  App model;
+  String next;
+  ObservableList territoryNames = FXCollections.observableArrayList();
 
-    @FXML ChoiceBox sourceTerritoryName;
+  @FXML ChoiceBox sourceTerritoryName;
 
-    @FXML ChoiceBox destTerritoryName;
+  @FXML ChoiceBox destTerritoryName;
 
-    @FXML Label territoryGroupName;
+  @FXML Label territoryGroupName;
 
-    @FXML Label errorMessage;
+  @FXML Label errorMessage;
 
-    // number of talents to move by type of talents
-    @FXML TextField numTalent1; // level0
+  // number of talents to move by type of talents
+  @FXML TextField numTalent1; // level0
 
-    @FXML TextField numTalent2; // level1
+  @FXML TextField numTalent2; // level1
 
-    @FXML TextField numTalent3;
+  @FXML TextField numTalent3;
 
-    @FXML TextField numTalent4;
+  @FXML TextField numTalent4;
 
-    @FXML TextField numTalent5;
+  @FXML TextField numTalent5;
 
-    @FXML TextField numTalent6;
+  @FXML TextField numTalent6;
 
-    @FXML TextField numTalent7; // level6
+  @FXML TextField numTalent7; // level6
 
-    @FXML ArrayList<Label> labelList;
+  @FXML ArrayList<Label> labelList;
 
-    @FXML
-    Label playerInfo;
+  @FXML Label playerInfo;
 
-    public MoveActionOddPlayersController(App model) {
-        this.model = model;
-        this.next = "selectActionOddPlayers";
+  public MoveActionOddPlayersController(App model) {
+    this.model = model;
+    this.next = "selectActionOddPlayers";
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    InitializeControllerHelper helper = new InitializeControllerHelper();
+    // set coloring for each territory label
+    helper.initializeTerritoryLabelByOwner(model.getPlayer().getMap(), labelList);
+    // set tooltip for each territory label
+    helper.initializeTerritoryTooltips(model.getPlayer().getMap(), labelList);
+    // set tooltip for player info
+    helper.initializePlayerInfoTooltip(
+        model.getPlayer().getMap(), model.getPlayer().getName(), playerInfo);
+    // set coloring for player info
+    helper.initializeTerritoryPlayerInfoColor(model, playerInfo);
+
+    setTerritoryNames();
+    territoryGroupName.setText("Move");
+    numTalent1.setText("0");
+    numTalent2.setText("0");
+    numTalent3.setText("0");
+    numTalent4.setText("0");
+    numTalent5.setText("0");
+    numTalent6.setText("0");
+    numTalent7.setText("0");
+  }
+
+  /* Fills the choice boxes with a list of territories that a player owns.
+   * After the choice boxes are filled, the player is able to select the source and destination
+   * territories for moving talents.
+   */
+  private void setTerritoryNames() {
+    territoryNames.removeAll(territoryNames);
+    String playerName = model.getPlayer().getName();
+    HashMap<String, Territory> playerTerritories =
+        model.getPlayer().getMap().getPlayerTerritories(playerName);
+    for (String territoryName : playerTerritories.keySet()) {
+      territoryNames.add(territoryName);
     }
+    sourceTerritoryName.getItems().addAll(territoryNames);
+    destTerritoryName.getItems().addAll(territoryNames);
+  }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        InitializeControllerHelper helper = new InitializeControllerHelper();
-        // set coloring for each territory label
-        helper.initializeTerritoryLabelByOwner(model.getPlayer().getMap(), labelList);
-        // set tooltip for each territory label
-        helper.initializeTerritoryTooltips(model.getPlayer().getMap(), labelList);
-        // set tooltip for player info
-        helper.initializePlayerInfoTooltip(model.getPlayer().getMap(), model.getPlayer().getName(), playerInfo);
+  public void onTypingNumUnits(KeyEvent ke) throws Exception {}
 
-        helper.initializeTerritoryGroupLabelColor(model, territoryGroupName);
-        setTerritoryNames();
-        territoryGroupName.setText("Move");
-        // territoryGroupName.setText(model.getPlayer().getTerritoryGroupSelected());
-        numTalent1.setText("0");
-        numTalent2.setText("0");
-        numTalent3.setText("0");
-        numTalent4.setText("0");
-        numTalent5.setText("0");
-        numTalent6.setText("0");
-        numTalent7.setText("0");
-    }
-
-    /* Fills the choice boxes with a list of territories that a player owns.
-     * After the choice boxes are filled, the player is able to select the source and destination
-     * territories for moving talents.
-     */
-    private void setTerritoryNames() {
-        territoryNames.removeAll(territoryNames);
-        String playerName = model.getPlayer().getName();
-        HashMap<String, Territory> playerTerritories =
-                model.getPlayer().getMap().getPlayerTerritories(playerName);
-        for (String territoryName : playerTerritories.keySet()) {
-            territoryNames.add(territoryName);
-        }
-        sourceTerritoryName.getItems().addAll(territoryNames);
-        destTerritoryName.getItems().addAll(territoryNames);
-    }
-
-    public void onTypingNumUnits(KeyEvent ke) throws Exception {}
-
-    public void onMove(ActionEvent ae) throws Exception {
-        Object source = ae.getSource();
-        if (source instanceof Button) {
-            String isValidInput =
-                    checkInput(); // make sure all the inputs are valid for the move order.
-            if (isValidInput == null) {
-                ActionInfoFactory af = new ActionInfoFactory();
-                HashMap<String, Integer> numUnits = getNumUnits();
-                ActionInfo info =
-                        af.createMoveActionInfo(
-                                model.getPlayer().getName(),
-                                (String) sourceTerritoryName.getValue(),
-                                (String) destTerritoryName.getValue(),
-                                numUnits);
-                String success = model.getPlayer().tryIssueMoveOrder(info);
-                if (success != null) {
-                    errorMessage.setText(success);
-                } else {
-                    loadNextPhase((Stage) (((Node) ae.getSource()).getScene().getWindow()));
-                }
-            } else {
-                errorMessage.setText(isValidInput);
-            }
+  public void onMove(ActionEvent ae) throws Exception {
+    Object source = ae.getSource();
+    if (source instanceof Button) {
+      String isValidInput = checkInput(); // make sure all the inputs are valid for the move order.
+      if (isValidInput == null) {
+        ActionInfoFactory af = new ActionInfoFactory();
+        HashMap<String, Integer> numUnits = getNumUnits();
+        ActionInfo info =
+            af.createMoveActionInfo(
+                model.getPlayer().getName(),
+                (String) sourceTerritoryName.getValue(),
+                (String) destTerritoryName.getValue(),
+                numUnits);
+        String success = model.getPlayer().tryIssueMoveOrder(info);
+        if (success != null) {
+          errorMessage.setText(success);
         } else {
-            throw new IllegalArgumentException("Invalid ActionEvent source " + source);
+          loadNextPhase((Stage) (((Node) ae.getSource()).getScene().getWindow()));
         }
+      } else {
+        errorMessage.setText(isValidInput);
+      }
+    } else {
+      throw new IllegalArgumentException("Invalid ActionEvent source " + source);
     }
+  }
 
-    /* Ensures all the inputs required to make a valid move are valid and returns a null string.
-     * Returns a string with a descriptive error if check fails.
-     */
-    String checkInput() {
-        try {
-            parseIntFromTextField(numTalent1.getText(), 1);
-            parseIntFromTextField(numTalent2.getText(), 2);
-            parseIntFromTextField(numTalent3.getText(), 3);
-            parseIntFromTextField(numTalent4.getText(), 4);
-            parseIntFromTextField(numTalent5.getText(), 5);
-            parseIntFromTextField(numTalent6.getText(), 6);
-            parseIntFromTextField(numTalent7.getText(), 7);
-            sourceTerritoryName.getValue();
-            destTerritoryName.getValue();
-        } catch (IllegalArgumentException iae) {
-            return iae.getMessage();
-        } catch (NullPointerException npe) {
-            return "Source and/or destination are empty.";
-        }
-        return null;
+  /* Ensures all the inputs required to make a valid move are valid and returns a null string.
+   * Returns a string with a descriptive error if check fails.
+   */
+  String checkInput() {
+    try {
+      parseIntFromTextField(numTalent1.getText(), 1);
+      parseIntFromTextField(numTalent2.getText(), 2);
+      parseIntFromTextField(numTalent3.getText(), 3);
+      parseIntFromTextField(numTalent4.getText(), 4);
+      parseIntFromTextField(numTalent5.getText(), 5);
+      parseIntFromTextField(numTalent6.getText(), 6);
+      parseIntFromTextField(numTalent7.getText(), 7);
+      sourceTerritoryName.getValue();
+      destTerritoryName.getValue();
+    } catch (IllegalArgumentException iae) {
+      return iae.getMessage();
+    } catch (NullPointerException npe) {
+      return "Source and/or destination are empty.";
     }
+    return null;
+  }
 
-    /* Returns the number of units requested by the user (by typing in the respective TextField boxes) to issue a move order.
-     */
-    HashMap<String, Integer> getNumUnits() throws IllegalArgumentException {
-        HashMap<String, Integer> numUnits = new HashMap<>();
-        numUnits.put("level0", parseIntFromTextField(numTalent1.getText(), 1));
-        numUnits.put("level1", parseIntFromTextField(numTalent2.getText(), 2));
-        numUnits.put("level2", parseIntFromTextField(numTalent3.getText(), 3));
-        numUnits.put("level3", parseIntFromTextField(numTalent4.getText(), 4));
-        numUnits.put("level4", parseIntFromTextField(numTalent5.getText(), 5));
-        numUnits.put("level5", parseIntFromTextField(numTalent6.getText(), 6));
-        numUnits.put("level6", parseIntFromTextField(numTalent6.getText(), 7));
-        return numUnits;
-    }
+  /* Returns the number of units requested by the user (by typing in the respective TextField boxes) to issue a move order.
+   */
+  HashMap<String, Integer> getNumUnits() throws IllegalArgumentException {
+    HashMap<String, Integer> numUnits = new HashMap<>();
+    numUnits.put("level0", parseIntFromTextField(numTalent1.getText(), 1));
+    numUnits.put("level1", parseIntFromTextField(numTalent2.getText(), 2));
+    numUnits.put("level2", parseIntFromTextField(numTalent3.getText(), 3));
+    numUnits.put("level3", parseIntFromTextField(numTalent4.getText(), 4));
+    numUnits.put("level4", parseIntFromTextField(numTalent5.getText(), 5));
+    numUnits.put("level5", parseIntFromTextField(numTalent6.getText(), 6));
+    numUnits.put("level6", parseIntFromTextField(numTalent6.getText(), 7));
+    return numUnits;
+  }
 
-    /* Returns an integer from text.
-     * @param text is the string from which integer is parsed.
-     * @param itemNumber is the index of the TextField. There are five TextField items 1, 2, 3, 4 and 5.
-     *        itemNumber is used for printing error messages if parsing fails.
-     */
-    private int parseIntFromTextField(String text, int itemNumber) throws IllegalArgumentException {
-        int parsedInt = 0;
-        try {
-            parsedInt = Integer.parseInt(text);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Integer cannot be parsed from " + text);
-        }
-        return parsedInt;
+  /* Returns an integer from text.
+   * @param text is the string from which integer is parsed.
+   * @param itemNumber is the index of the TextField. There are five TextField items 1, 2, 3, 4 and 5.
+   *        itemNumber is used for printing error messages if parsing fails.
+   */
+  private int parseIntFromTextField(String text, int itemNumber) throws IllegalArgumentException {
+    int parsedInt = 0;
+    try {
+      parsedInt = Integer.parseInt(text);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Integer cannot be parsed from " + text);
     }
+    return parsedInt;
+  }
 
-    /* Loads next phase after a player clicks on a menu item inside a SplitMenuButton.
-     * @param window is the source Stage where the user interacted.
-     */
-    private void loadNextPhase(Stage window) throws IOException {
-        Object controller = new ControllerFactory().getController(next, model);
-        Stage newWindow = PhaseChanger.switchTo(window, controller, next);
-        newWindow.show();
-    }
+  /* Loads next phase after a player clicks on a menu item inside a SplitMenuButton.
+   * @param window is the source Stage where the user interacted.
+   */
+  private void loadNextPhase(Stage window) throws IOException {
+    Object controller = new ControllerFactory().getController(next, model);
+    Stage newWindow = PhaseChanger.switchTo(window, controller, next);
+    newWindow.show();
+  }
 }
--- client/src/main/java/edu/duke/ece651/risk/client/controller/SelectActionController.java
+++ client/src/main/java/edu/duke/ece651/risk/client/controller/SelectActionController.java
@@ -18,149 +18,146 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class SelectActionController implements Initializable {
-    App model;
-    String next;
-    String nextOnCompleteTurn;
-    String nextOnLeave;
-    String nextOnMoveAction;
-    String nextOnAttackAction;
-    String nextOnUpgradeTalentsAction;
-    String nextOnUpgradeTechAction;
+  App model;
+  String next;
+  String nextOnCompleteTurn;
+  String nextOnLeave;
+  String nextOnMoveAction;
+  String nextOnAttackAction;
+  String nextOnUpgradeTalentsAction;
+  String nextOnUpgradeTechAction;
 
-    @FXML
-    Label playerInfo;
+  @FXML Label playerInfo;
 
-    @FXML
-    SplitMenuButton actionMenu;
+  @FXML SplitMenuButton actionMenu;
 
-    @FXML
-    ArrayList<Label> labelList;
+  @FXML ArrayList<Label> labelList;
 
-    /* Constructor that initializes the model and the next view.
-     */
-    public SelectActionController(App model) {
-        this.model = model;
+  /* Constructor that initializes the model and the next view.
+   */
+  public SelectActionController(App model) {
+    this.model = model;
+  }
+
+  /* Sets various labels in the view to help player understand their status in the game.
+   */
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    InitializeControllerHelper helper = new InitializeControllerHelper();
+    // set coloring for each territory label
+    helper.initializeTerritoryLabelByOwner(model.getPlayer().getMap(), labelList);
+    // set tooltip for each territory label
+    helper.initializeTerritoryTooltips(model.getPlayer().getMap(), labelList);
+    // set tooltip for player info
+    helper.initializePlayerInfoTooltip(
+        model.getPlayer().getMap(), model.getPlayer().getName(), playerInfo);
+    // set coloring for player info
+    helper.initializeTerritoryPlayerInfoColor(model, playerInfo);
+
+    if (model.getPlayer().getMap().getNumPlayers() % 2 == 0) {
+      this.nextOnCompleteTurn = "selectActionEvenPlayers";
+      this.nextOnMoveAction = "moveActionEvenPlayers";
+      this.nextOnAttackAction = "attackActionEvenPlayers";
+      this.nextOnUpgradeTalentsAction = "upgradeTalentsActionEvenPlayers";
+      this.nextOnUpgradeTechAction = "upgradeTechActionEvenPlayers";
+    } else {
+      this.nextOnCompleteTurn = "selectActionOddPlayers";
+      this.nextOnMoveAction = "moveActionOddPlayers";
+      this.nextOnAttackAction = "attackActionOddPlayers";
+      this.nextOnUpgradeTalentsAction = "upgradeTalentsActionOddPlayers";
+      this.nextOnUpgradeTechAction = "upgradeTechActionOddPlayers";
     }
+    this.nextOnLeave = "joinRoom";
+  }
 
-    /* Sets various labels in the view to help player understand their status in the game.
-     */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        InitializeControllerHelper helper = new InitializeControllerHelper();
-        // set coloring for each territory label
-        helper.initializeTerritoryLabelByOwner(model.getPlayer().getMap(), labelList);
-        // set tooltip for each territory label
-        helper.initializeTerritoryTooltips(model.getPlayer().getMap(), labelList);
-        // set tooltip for player info
-        helper.initializePlayerInfoTooltip(model.getPlayer().getMap(), model.getPlayer().getName(), playerInfo);
-
-        if (model.getPlayer().getMap().getNumPlayers() % 2 == 0) {
-            this.nextOnCompleteTurn = "selectActionEvenPlayers";
-            this.nextOnMoveAction = "moveActionEvenPlayers";
-            this.nextOnAttackAction = "attackActionEvenPlayers";
-            this.nextOnUpgradeTalentsAction = "upgradeTalentsActionEvenPlayers";
-            this.nextOnUpgradeTechAction = "upgradeTechActionEvenPlayers";
-        }
-        else {
-            this.nextOnCompleteTurn = "selectActionOddPlayers";
-            this.nextOnMoveAction = "moveActionOddPlayers";
-            this.nextOnAttackAction = "attackActionOddPlayers";
-            this.nextOnUpgradeTalentsAction = "upgradeTalentsActionOddPlayers";
-            this.nextOnUpgradeTechAction = "upgradeTechActionOddPlayers";
-        }
-        this.nextOnLeave = "joinRoom";
+  /* Opens a new view that asks user to move their talents within their territories.
+   */
+  public void onSelectMove(ActionEvent ae) throws Exception {
+    Object source = ae.getSource();
+    if (source instanceof MenuItem) {
+      next = nextOnMoveAction;
+      loadNextPhase((Stage) actionMenu.getScene().getWindow());
+    } else {
+      throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid.");
     }
+  }
 
-    /* Opens a new view that asks user to move their talents within their territories.
-     */
-    public void onSelectMove(ActionEvent ae) throws Exception {
-        Object source = ae.getSource();
-        if (source instanceof MenuItem) {
-            next = nextOnMoveAction;
-            loadNextPhase((Stage) actionMenu.getScene().getWindow());
-        } else {
-            throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid.");
-        }
+  /* Opens a new view that asks user to attack enemy territory.
+   */
+  public void onSelectAttack(ActionEvent ae) throws Exception {
+    Object source = ae.getSource();
+    if (source instanceof MenuItem) {
+      next = nextOnAttackAction;
+      loadNextPhase((Stage) actionMenu.getScene().getWindow());
+    } else {
+      throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid.");
     }
+  }
 
-    /* Opens a new view that asks user to attack enemy territory.
-     */
-    public void onSelectAttack(ActionEvent ae) throws Exception {
-        Object source = ae.getSource();
-        if (source instanceof MenuItem) {
-            next = nextOnAttackAction;
-            loadNextPhase((Stage) actionMenu.getScene().getWindow());
-        } else {
-            throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid.");
-        }
+  /* Opens a new view that asks user to upgrade their talents.
+   */
+  public void onSelectUpgradeTalents(ActionEvent ae) throws Exception {
+    Object source = ae.getSource();
+    if (source instanceof MenuItem) {
+      next = nextOnUpgradeTalentsAction;
+      loadNextPhase((Stage) actionMenu.getScene().getWindow());
+    } else {
+      throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid.");
     }
+  }
 
-    /* Opens a new view that asks user to upgrade their talents.
-     */
-    public void onSelectUpgradeTalents(ActionEvent ae) throws Exception {
-        Object source = ae.getSource();
-        if (source instanceof MenuItem) {
-            next = nextOnUpgradeTalentsAction;
-            loadNextPhase((Stage) actionMenu.getScene().getWindow());
-        } else {
-            throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid.");
-        }
+  /* Opens a new view that asks user to upgrade their technology level.
+   */
+  public void onSelectUpgradeTech(ActionEvent ae) throws Exception {
+    Object source = ae.getSource();
+    if (source instanceof MenuItem) {
+      next = nextOnUpgradeTechAction;
+      loadNextPhase((Stage) actionMenu.getScene().getWindow());
+    } else {
+      throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid.");
     }
+  }
 
-    /* Opens a new view that asks user to upgrade their technology level.
-     */
-    public void onSelectUpgradeTech(ActionEvent ae) throws Exception {
-        Object source = ae.getSource();
-        if (source instanceof MenuItem) {
-            next = nextOnUpgradeTechAction;
-            loadNextPhase((Stage) actionMenu.getScene().getWindow());
-        } else {
-            throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid.");
-        }
+  /* Switches to the view that asks player join a  room.
+   */
+  @FXML
+  public void onLeavingGame(ActionEvent ae) throws Exception {
+    Object source = ae.getSource();
+    if (source instanceof Button) { // go to join room
+      model.requestLeave();
+      next = nextOnLeave;
+      loadNextPhase((Stage) (((Node) ae.getSource()).getScene().getWindow()));
+    } else {
+      throw new IllegalArgumentException(
+          "Action event " + ae.getSource() + " is invalid for onAction().");
     }
+  }
 
-    /* Switches to the view that asks player join a  room.
-     */
-    @FXML
-    public void onLeavingGame(ActionEvent ae) throws Exception {
-        Object source = ae.getSource();
-        if (source instanceof Button) { // go to join room
-            model.requestLeave();
-            next = nextOnLeave;
-            loadNextPhase((Stage) (((Node) ae.getSource()).getScene().getWindow()));
-        }
-        else {
-            throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid for onAction().");
-        }
+  /* Takes the user to a view that asks them to wait for other players to finish their turn.
+   */
+  @FXML
+  public void onCompletingTurn(ActionEvent ae) throws Exception {
+    Object source = ae.getSource();
+    if (source instanceof Button) {
+      String status = model.getPlayer().doneIssueOrders();
+      if (status != null) {
+        // show message in new screen
+      } else {
+        this.next = nextOnCompleteTurn;
+        loadNextPhase((Stage) (((Node) ae.getSource()).getScene().getWindow()));
+      }
+    } else {
+      throw new IllegalArgumentException(
+          "Action event " + ae.getSource() + " is invalid for onAction().");
     }
+  }
 
-    /* Takes the user to a view that asks them to wait for other players to finish their turn.
-     */
-    @FXML
-    public void onCompletingTurn(ActionEvent ae) throws Exception {
-        Object source = ae.getSource();
-        if (source instanceof Button) {
-            String status = model.getPlayer().doneIssueOrders();
-            if (status != null) {
-                // show message in new screen
-            }
-            else {
-                this.next = nextOnCompleteTurn;
-                loadNextPhase((Stage) (((Node) ae.getSource()).getScene().getWindow()));
-            }
-        }
-        else {
-            throw new IllegalArgumentException("Action event " + ae.getSource() + " is invalid for onAction().");
-        }
-    }
-
-    /* Loads next phase after a player clicks on a menu item inside a SplitMenuButton.
-     * @param window is the source Stage where the user interacted.
-     */
-    private void loadNextPhase(Stage window) throws IOException {
-        Object controller = new ControllerFactory().getController(next, model);
-        Stage newWindow = PhaseChanger.switchTo(window, controller, next);
-        newWindow.show();
-    }
-
+  /* Loads next phase after a player clicks on a menu item inside a SplitMenuButton.
+   * @param window is the source Stage where the user interacted.
+   */
+  private void loadNextPhase(Stage window) throws IOException {
+    Object controller = new ControllerFactory().getController(next, model);
+    Stage newWindow = PhaseChanger.switchTo(window, controller, next);
+    newWindow.show();
+  }
 }
