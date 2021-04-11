4a5313cc4bd766db638a5111b28fc6b7334b75a9
yz566 <yz566@duke.edu>
2021-04-11 00:24:11 -0400

    documentation and test for CloakingActionInfo

--- shared/src/main/java/edu/duke/ece651/risk/shared/CloakingActionInfo.java
+++ shared/src/main/java/edu/duke/ece651/risk/shared/CloakingActionInfo.java
@@ -1,20 +1,39 @@
 package edu.duke.ece651.risk.shared;
 
 public class CloakingActionInfo implements java.io.Serializable {
+    /** The String that represents the target territory, on which to do cloaking. */
     private String targetTerritoryName;
 
+    /** The default constructor of CloakingActionInfo. */
     public CloakingActionInfo() {
         this.targetTerritoryName = null;
     }
 
+    /**
+     * Constructs an CloakingActionInfo with a given target territory name
+     *
+     * @param targetTerritoryName a String that represents the target territory, on which to do
+     *     cloaking.
+     */
     public CloakingActionInfo(String targetTerritoryName) {
         this.targetTerritoryName = targetTerritoryName;
     }
 
+    /**
+     * Getter of the targetTerritoryName.
+     *
+     * @return a String that represents the target territory, on which to do * cloaking.
+     */
     public String getTargetTerritoryName() {
         return targetTerritoryName;
     }
 
+    /**
+     * Setter of the targetTerritoryName.
+     *
+     * @param targetTerritoryName a String that represents the target territory, on which to do *
+     *     cloaking.
+     */
     public void setTargetTerritoryName(String targetTerritoryName) {
         this.targetTerritoryName = targetTerritoryName;
     }
