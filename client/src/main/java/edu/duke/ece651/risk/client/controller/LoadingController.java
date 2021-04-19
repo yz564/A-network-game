a37555498a7bbe0409cfd7ed093d26ad3fb1d1a3
jx97 <jy.xu@duke.edu>
2021-04-19 14:12:24 -0400

    trying with string binding

--- client/src/main/java/edu/duke/ece651/risk/client/controller/LoadingController.java
+++ client/src/main/java/edu/duke/ece651/risk/client/controller/LoadingController.java
@@ -4,6 +4,7 @@ import edu.duke.ece651.risk.client.App;
 import edu.duke.ece651.risk.client.ClientEvent;
 import edu.duke.ece651.risk.client.ClientEventListener;
 import edu.duke.ece651.risk.client.GUIEventMessenger;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -20,6 +21,8 @@ public class LoadingController extends Controller implements Initializable, Clie
     @FXML Label loadingMessage;
 
     GUIEventMessenger messenger;
+
+    Boolean isUpdated = false;
 
     /**
      * Defines the model and the next phase to load.
@@ -42,6 +45,25 @@ public class LoadingController extends Controller implements Initializable, Clie
     public void initialize(URL location, ResourceBundle resources) {
         // model.getPlayer().waitOtherPlayers();
         model.getMessenger().setClientEventListener(this);
+        loadingMessage.textProperty().bind(Bindings.createStringBinding(()->{
+            String s = " ";
+            if (isUpdated){
+                s = "All players ready!!";
+            }
+            else{
+                s = " Wait for other players";
+            }
+            return s;
+        }));
+        loadingMessage.textProperty()
+                .addListener(
+                        (observable, oldValue, newValue) -> {
+                            try {
+                                loadNextPhase((Stage) loadingMessage.getScene().getWindow());
+                            } catch (IOException e) {
+                                loadingMessage.setText(e.getMessage());
+                            }
+                        });
     }
 
     @Override
@@ -49,7 +71,8 @@ public class LoadingController extends Controller implements Initializable, Clie
     public void onUpdateEvent(ClientEvent ce) throws Exception {
         // gets new map from initialization.
         this.next = "selectTerritoryGroup";
-        loadNextPhase((Stage) loadingMessage.getScene().getWindow());
+        isUpdated = true;
+        //loadNextPhase((Stage) loadingMessage.getScene().getWindow());
     }
 
     public void onMouseMoved(MouseEvent mouseEvent) {
