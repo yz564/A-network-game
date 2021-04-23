bf912128cee6eb3802ee40a1adeb927194c059fc
Yang Zhong <yz564@duke.edu>
2021-04-23 12:54:30 -0400

    add watch game in client

--- client/src/main/java/edu/duke/ece651/risk/client/App.java
+++ client/src/main/java/edu/duke/ece651/risk/client/App.java
@@ -34,7 +34,6 @@ public class App implements Runnable, GUIEventListener {
     private GUIEvent theGUIEvent;
     private Boolean isGUIUpdated;
     private ClientEventMessenger messenger;
-    private ControllerFactory myFactory;
     private Boolean isLeave;
     private Boolean isRejoin;
 
@@ -126,39 +125,6 @@ public class App implements Runnable, GUIEventListener {
         out.flush();
         out.reset();
         isLeave = true;
-    }
-
-    /** every time after join a room, call this method */
-    public void runOnePlayer() throws Exception {
-        String tmpS;
-        checkIn();
-        // if Player.wait is true, the System.in read at here. Otherwise, the System.in read in
-        // player
-        // thread
-
-        players.get(currentRoomId).setWait(true);
-        while (true) {
-            if (players.get(currentRoomId).isWait()) {
-                players.get(currentRoomId).ready =
-                        true; // once arrive here (let the main thread listen instead of let the
-                // player thread
-                // listen), set player.ready = true, tell the player to set player.wait=false.
-                if ((tmpS = stdIn.readLine())
-                        != null) { // arrive here only when player.wait is true
-                    if (tmpS.equals(
-                            "/leave")) { // if request leave, tell the server and go back join room
-                        // page
-                        requestLeave();
-                        break;
-                    }
-                    players.get(currentRoomId)
-                            .updateInput(
-                                    tmpS); // if not request leave, pass what the client get from
-                    // system.in to the
-                    // player.tmpS
-                }
-            }
-        }
     }
 
     public ObjectIO receiveMessage() throws Exception {
@@ -261,7 +227,11 @@ public class App implements Runnable, GUIEventListener {
                     String result = getPlayer().checkStatus();
                     if (result == null) {
                         messenger.setMap(getPlayer().getMap(), "selectAction");
-                    } else {
+                    }
+                    else if (result.equals("You lose")) {
+                      messenger.setMap(getPlayer().getMap(), "watchGame");
+                    }
+                    else {
                         messenger.setMap(getPlayer().getMap(), "gameEnd");
                         break;
                     }
@@ -281,82 +251,4 @@ public class App implements Runnable, GUIEventListener {
         System.out.println("isGUIUpdated: " + isGUIUpdated);
         System.out.println("App: onUpdateEvent");
     }
-    /*
-      @Override
-      public void onUpdateJoinRoom(GUIEvent ge){
-        try{
-          receiveMessage();
-          int roomId=ge.getRoomId();
-          currentRoomId = roomId - 1;
-          sendMessage(new ObjectIO("", roomId));
-          tmp = receiveMessage();
-          // messenger.setClientEventListener((ClientEventListener)myFactory.getController("joinRoom",this));
-          messenger.setStatusBoolean(tmp.id==0);
-          System.out.println("messenger status");
-          /*
-          sendMessage(new ObjectIO("wait others", 0));
-        tmp = receiveMessage();
-
-        System.out.println(tmp.message);
-        getPlayer().receiveMessage();
-        messenger.setClientEventListener((ClientEventListener)myFactory.getController("loading",this));
-        messenger.setMap(getPlayer().getMap());
-        }
-        catch (Exception e) {
-          System.out.println("Exception catched 1: "+e.getMessage());
-        }
-      }
-
-      @Override
-      public void onUpdateWaitOthers(GUIEvent ge){
-        try{
-        sendMessage(new ObjectIO("wait others", 0));
-        tmp = receiveMessage();
-        System.out.println(tmp.message);
-        getPlayer().receiveMessage();
-        //messenger.setClientEventListener((ClientEventListener)myFactory.getController("loading",this));
-        messenger.setMap(getPlayer().getMap());
-
-      } catch (Exception e) {
-          System.out.println("Exception catched 2: "+e.getMessage());
-        }
-      }
-    */
-    /**
-     * the enter point of the client. after connecting with the server, new App, and call its method
-     * to communicate with the server(game).
-     */
-    /*
-    public static void main(String[] args) throws Exception {
-      System.out.println("Please enter server address: (default is localhost by hitting Enter)");
-      BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-      String ServerAddress = stdIn.readLine();
-      if (ServerAddress.equals("")) {
-        ServerAddress = "localhost";
-      }
-      int portNumber = 3333;
-      try (var server = new Socket(ServerAddress, portNumber)) {
-        ObjectOutputStream out = new ObjectOutputStream(server.getOutputStream());
-        ObjectInputStream in = new ObjectInputStream(server.getInputStream());
-        ObjectIO tmp = null;
-        App client = new App(server, in, out, tmp);
-        String userName = "";
-        String password = "";
-        while (!client.tryLogin(userName, password)) {
-          System.out.println("userName:");
-          userName = stdIn.readLine();
-          System.out.println("password:");
-          password = stdIn.readLine();
-        }
-        while (true) {
-          int roomId = 0;
-          while (!client.tryJoinRoom(roomId)) {
-            System.out.println("join a room:");
-            roomId = Integer.parseInt(stdIn.readLine());
-          }
-          client.runOnePlayer();
-        }
-      } // catch (Exception e) {
-      // }
-      }*/
 }
