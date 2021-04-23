d6c9e5c329ec8ba989b5c401f3112eb60553ed24
Amandeep Rathee <ar543@duke.edu>
2021-04-23 13:35:13 -0400

    run server on vm for testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-./gradlew build || exit 1
+cd server && ./gradlew installDist || exit 1
+cd ..
+server/build/install/server/bin/server &
+sleep 20
+cd client && ./gradlew build || exit 1
 ./gradlew cloverAggregateReports || exit 1
 scripts/coverage_summary.sh
 ls -l /
 ls -l /coverage-out/
 cp -r build/reports/clover/html/* /coverage-out/ || exit 1
+killall server
