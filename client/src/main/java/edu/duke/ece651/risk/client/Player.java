ae70bdf04b1c08dc0fb7095f6f08a0f6b1bba822
yz566 <yz566@duke.edu>
2021-04-23 23:33:06 -0400

    deleted ClientOrderHelper

--- client/src/main/java/edu/duke/ece651/risk/client/Player.java
+++ client/src/main/java/edu/duke/ece651/risk/client/Player.java
@@ -130,66 +130,6 @@ public class Player implements Runnable {
         return false;
     }
 
-    /**
-     * first wait to read the ObjectIO sent by the server. then let the user to select the available
-     * group finally send the ObjectIO with the selection to the server.
-     */
-    /*
-    public void doInitialization() throws Exception {
-      while (true) {
-        tmpS = stdIn.readLine();
-        String str = tryInitialization(tmpS);
-        System.out.println(str);
-        if (str == null) {
-          break;
-        }
-
-      }
-    }*/
-    /*
-      public void doInitialization(Boolean b) throws Exception {
-          if (b) {
-              if ((tmp = (ObjectIO) in.readObject()) != null) {}
-          }
-          mark:
-          while (true) {
-              wait = true; // let the main thread to listen input just once
-              while (!ready) {}
-              wait = false;
-              ready = false;
-              tmpS = null;
-              System.out.println(tmp.message);
-              System.out.println("(you can leave the game by /leave) Your available choices are: ");
-              Iterator<Integer> itr = (tmp.groups).iterator();
-              while (itr.hasNext()) {
-                  Integer g = (Integer) itr.next();
-                  System.out.println(Integer.toString(g) + " : " + tmp.map.getInitGroup(g));
-              }
-              // if ((tmpS = stdIn.readLine()) != null) {}
-              while (tmpS == null) {
-                  if (wait) {
-                      continue mark;
-                  }
-              }
-              try {
-                  if (tmp.groups.contains(Integer.parseInt(tmpS))) {
-                      break;
-                  }
-              } catch (NumberFormatException e) {
-                  System.out.println("Input should be a number, please retry");
-              }
-              System.out.println("Your input is not valid, please retry");
-          }
-          out.writeObject(new ObjectIO(tmpS, Integer.parseInt(tmpS)));
-          out.flush();
-          out.reset();
-          tmpS = null;
-          if ((tmp = (ObjectIO) in.readObject()) != null) {}
-          if (tmp.id == -1) {
-              doInitialization(false);
-          }
-      }
-    */
     public void startAllocation() throws Exception {
         receiveMessage();
         receiveMessage();
@@ -405,88 +345,6 @@ public class Player implements Runnable {
         }
         return null;
     }
-    /*
-      public void doPlacement() throws Exception {
-          System.out.println("-----waitServerInput-----");
-          if ((tmp = (ObjectIO) in.readObject()) != null) {
-              String playerName = tmp.message;
-              ClientOrderHelper coh =
-                      new ClientOrderHelper(playerName, stdIn, new PrintStream(System.out));
-              out.writeObject(
-                      coh.issuePlaceOrders(
-                              tmp.id, tmp.playerNames)); // here tmp.playerNames is territory names...
-          }
-      }
-
-      public void doAskLeave() {
-          tmpS = null;
-          System.out.println("you can leave by /leave, or continue by entering anything else");
-          wait = true;
-          while (!ready) {}
-          wait = false;
-          ready = false;
-          while (tmpS == null) {
-              if (wait) {
-                  System.out.println("Return to the game...");
-                  wait = false;
-                  tmpS = null;
-                  doAskLeave();
-              }
-          }
-      }
-    */
-    /**
-     * first wait the ObjectIO from server, then call the placeOrder method in the helper class,
-     * finally send ObjectIO to server need to check the status of the player: win or lose
-     */
-    /*
-        public void doAction() throws Exception {
-            while (true) {
-                // doAskLeave();
-
-                System.out.println("waiting other players");
-                // stdIn = new BufferedReader(new InputStreamReader(System.in));
-                if ((tmp = (ObjectIO) in.readObject()) != null) {
-                    if (tmp.id < 0) {
-                        break;
-                    }
-                    doAskLeave();
-                    // MapTextView mapview = new MapTextView(tmp.playerNames);
-                    // System.out.println(mapview.displayMap(tmp.map));
-                    // System.out.println(tmp.message);
-                    String playerName = tmp.message;
-                    ClientOrderHelper coh =
-                            new ClientOrderHelper(playerName, stdIn, new PrintStream(System.out));
-                    out.writeObject(coh.issueActionOrders(tmp.map, tmp.playerNames));
-                }
-                // doAskLeave();
-                // stdIn = null;
-                tmpS = null;
-                System.out.println("you can leave by /leave, continue by entering anything else");
-                wait = true;
-                while (!ready) {
-                }
-                wait = false;
-                ready = false;
-                while (tmpS == null) {
-                    if (wait) {
-                      System.out.println("Return to the game...");
-                      wait = false;
-                      continue mark2;
-                    }
-                  }
-            }
-            if (tmp.id == -1) {
-                System.out.println("Your lost all territories...");
-            }
-            if (tmp.id == -2) {
-                System.out.println(tmp.message);
-            }
-            if (tmp.id == -3) {
-                System.out.println("You win!");
-            }
-        }
-    */
 
     /**
      * each turn ask the user watch or not, enter something start with /q will quit, no longer print
@@ -495,25 +353,7 @@ public class Player implements Runnable {
     public void doWatch() throws Exception {
         receiveMessage();
     }
-    /*
-        public void doWatch() throws Exception {
-            while (true) {
-                if ((tmp = (ObjectIO) in.readObject()) != null) {
-                    MapTextView mapview = new MapTextView(tmp.playerNames);
-                    System.out.println(mapview.displayMap(tmp.map));
-                    System.out.println(tmp.message);
-                }
-                String tmpstr;
-                System.out.println("Do you want watch? you can quit by /q");
-                if ((tmpstr = stdIn.readLine()) != null) {
-                    if (tmpstr.toLowerCase().startsWith("/q")) {
-                        System.out.println("quited");
-                        break;
-                    }
-                }
-            }
-        }
-    */
+
     @Override
     public void run() {
         try {
